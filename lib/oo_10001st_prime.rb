3c2b039522ed58b37a370d0967ad60048a0496d6
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2020-08-15 16:47:01 +0000

    Done.

--- lib/oo_10001st_prime.rb
+++ lib/oo_10001st_prime.rb
@@ -1,1 +1,26 @@
 # Implement your object-oriented solution here!
+
+class Prime
+  attr_reader :number
+
+  def initialize(limit)
+    @limit = limit
+    @number = number
+  end
+
+  def is_prime?(n)
+    (2..Math.sqrt(n).round).each{|x| return false if n % x == 0}
+    true
+  end
+
+
+  def number
+    n = 1
+    i = 0
+    while i != @limit
+      n += 1
+      i += 1 if is_prime?(n)
+    end
+    n
+  end
+end
