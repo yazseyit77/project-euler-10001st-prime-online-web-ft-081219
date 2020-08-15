a0e1ace52049abca07eb49fe88f2dd2dcce0a655
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2020-08-15 16:45:46 +0000

    Automatically backed up by Learn

--- lib/oo_10001st_prime.rb
+++ lib/oo_10001st_prime.rb
@@ -1,1 +1,15 @@
 # Implement your object-oriented solution here!
+
+class IsPrime
+  att_reader :number
+
+  def initialize(limit)
+    @limit = limit
+    @number = number
+  end
+
+  def is_prime?(n)
+
+  end
+
+end
