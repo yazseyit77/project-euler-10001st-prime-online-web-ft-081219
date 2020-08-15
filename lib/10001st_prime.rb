31c5ea77e21a7cec502c4c158a20e5cbb0dc125b
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2020-08-15 16:44:28 +0000

    Automatically backed up by Learn

--- lib/10001st_prime.rb
+++ lib/10001st_prime.rb
@@ -4,3 +4,14 @@ def is_prime?(n)
   (2..Math.sqrt(n).round).each{|x| return false if n % x == 0}
   true
 end
+
+
+def prime_number_for(limit)
+  n = 1
+  i = 0
+  while i != limit
+    n += 1
+    i += 1 if is_prime?(n)
+  end
+  
+end
